7b24005f2f52777af3874481b0731dc17d2584dc
Felipe Ramos <f3liperamos@gmail.com>
2021-01-25 23:50:29 +0100

    adding a doc test

--- rust/exercises/src/lib.rs
+++ rust/exercises/src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+/// Function takes a number and adds 2
+///
+/// # Basic usage
+///
+/// ```
+/// let three = exercises::adds_two(1);
+/// assert_eq!(three, 3);
+/// ```
+
+pub fn adds_two(n: usize) -> usize {
+    n + 2
 }
